680b00ceef5f18a7c33456dc8baec9fc4924ae7b
Dan Hermann <dh78@me.com>
2016-12-10 10:16:23 -0600

    Revert "Passport Install"
    This reverts commit 2ac9ee7a52ee2568703f38583a8f6dbf30afae6a.

--- config/app.php
+++ config/app.php
@@ -178,8 +178,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Laravel\Passport\PassportServiceProvider::class,
-
     ],
 
     /*
--- database/migrations/2014_10_12_100000_create_accounts_table.php
+++ database/migrations/2014_10_12_100000_create_accounts_table.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAccountsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+        Schema::create('accounts', function (Blueprint $table) {
             $table->string('email')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->tinyInteger('is_active');
+            $table->string('token')->nullable();
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::drop('accounts');
     }
 }

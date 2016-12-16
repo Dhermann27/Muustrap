78352d531e1d726708d996d31fc8ba9a8ce6595e
Dan Hermann <dh78@me.com>
2016-12-15 22:16:12 -0600

    Make Auth

--- database/migrations/2014_10_12_100000_create_users_table.php
+++ database/migrations/2014_10_12_100000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAccountsTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateAccountsTable extends Migration
      */
     public function up()
     {
-        Schema::create('accounts', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
             $table->string('email')->unique();
             $table->string('password');
-            $table->tinyInteger('is_active');
-            $table->string('token')->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateAccountsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('accounts');
+        Schema::dropIfExists('users');
     }
 }

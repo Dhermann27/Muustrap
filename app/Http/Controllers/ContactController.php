a5af3702aee7440fe6a3b4ba530d779f3ab9cf6f
Dan Hermann <dh78@me.com>
2017-03-06 14:42:19 -0600

    Emergency contacts fix

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Mail\ArtFair;
+use App\Mail\ContactUs;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;

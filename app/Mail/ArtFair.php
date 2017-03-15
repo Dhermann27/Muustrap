cd4b97746ae92210cabcd0951d28ff4c3e291f65
Dan Hermann <dh78@me.com>
2017-03-14 20:53:15 -0500

    Art Fair No images fix

--- app/Mail/ArtFair.php
+++ app/Mail/ArtFair.php
@@ -33,11 +33,13 @@ class ArtFair extends Mailable
     public function build()
     {
         $view = $this->from($this->camper->email)->view('mail.artfair');
-        foreach($this->request->images as $index=>$image) {
-            $view->attach($image, [
-                'as' => $this->camper->lastname . $index . "." . $image->extension(),
-                'mime' => $image->getMimeType(),
-            ]);
+        if (count($this->request->images) > 0) {
+            foreach ($this->request->images as $index => $image) {
+                $view->attach($image, [
+                    'as' => $this->camper->lastname . $index . "." . $image->extension(),
+                    'mime' => $image->getMimeType(),
+                ]);
+            }
         }
         return $view;
     }

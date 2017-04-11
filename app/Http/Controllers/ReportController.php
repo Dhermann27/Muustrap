927c8f1d767d74209fdc1f6429727610570956be
Dan Hermann <dh78@me.com>
2017-04-11 12:54:30 -0500

    Bank Deposits

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,16 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class ReportController extends Controller
 {
-    public function campers() {
+    public function campers()
+    {
         $years = \App\Byyear_Family::with('campers')->where('year', '>', DB::raw('getcurrentyear()-8'))
             ->orderBy('year')->orderBy('name')->get()->groupBy('year');
         return view('reports.campers', ['years' => $years]);
     }
 
-    public function rates() {
+    public function depositsMark($id) {
+        \App\Charge::where('chargetypeid', $id)->where('deposited_date', null)
+            ->update(['deposited_date' => Carbon::now()->toDateString()]);
+        return $this->deposits();
+    }
+
+    public function deposits()
+    {
+        $chargetypes = \App\Chargetype::where('is_deposited', '1')->get();
+        return view('reports.deposits', ['chargetypes' => $chargetypes,
+            'charges' => \App\Thisyear_Charge::whereIn('chargetypeid', $chargetypes->pluck('id')->toArray())
+                ->orderBy('deposited_date')->orderBy('timestamp', 'desc')->get()->groupBy('deposited_date')
+        ]);
+    }
+
+    public function rates()
+    {
         $year = \App\Year::where('is_current', 1)->first()->year;
         return view('reports.rates', ['buildings' => \App\Building::all(),
             'programs' => \App\Program::where('start_year', '<=', $year)->where('end_year', '>=', $year)
@@ -20,7 +38,8 @@ class ReportController extends Controller
             'rates' => \App\Rate::where('start_year', '<=', $year)->where('end_year', '>=', $year)->get()]);
     }
 
-    public function rooms() {
+    public function rooms()
+    {
         $years = \App\Byyear_Camper::where('year', '>', DB::raw('getcurrentyear()-5'))
             ->whereNotNull('roomid')->orderBy('year')->orderBy('room_number')->get()->groupBy('year');
         return view('reports.rooms', ['years' => $years, 'buildings' => \App\Building::all()]);

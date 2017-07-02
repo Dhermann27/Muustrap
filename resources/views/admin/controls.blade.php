a7bb396f617dc8db2c9c8d66931ebe0e3b9db55c
Dan Hermann <dh78@me.com>
2017-07-02 13:30:41 -0500

    Controls Font Awesome

--- resources/views/admin/controls.blade.php
+++ resources/views/admin/controls.blade.php
@@ -2,15 +2,15 @@
     <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
             id="controls{{ $id }}" aria-expanded="false">Details <span class="caret"></span></button>
     <ul class="dropdown-menu" role="menu" aria-labelledby="controls{{ $id }}">
-        <li><a href="{{ url('/household/' . $id) }}" class="fa fa-home">Household</a></li>
-        <li><a href="{{ url('/camper/' . $id) }}" class="fa fa-group"> Campers</a></li>
-        <li><a href="{{ url('/payment/' . $id) }}" class="fa fa-money"> Statement</a></li>
-        <li><a href="{{ url('/workshopchoice/' . $id) }}" class="fa fa-rocket"> Workshops</a></li>
-        <li><a href="{{ url('/roomselection/' . $id) }}" class="fa fa-bed"> Room</a></li>
-        <li><a href="{{ url('/volunteer/' . $id) }}" class="fa fa-handshake-o"> Volunteer</a></li>
-        <li><a href="{{ url('/confirm/' . $id) }}" class="fa fa-envelope"> Confirmation</a></li>
-        <li><a href="{{ url('/nametag/' . $id) }}" class="fa fa-id-card"> Customize Nametags</a></li>
-        <li><a href="{{ url('/nametags/' . $id) }}" class="fa fa-print"> Print Nametags</a></li>
-        <li><a href="{{ url('/calendar/' . $id) }}" class="fa fa-calendar"> Calendar</a></li>
+        <li><a href="{{ url('/household/' . $id) }}"><i class="fa fa-home"></i> Household</a></li>
+        <li><a href="{{ url('/camper/' . $id) }}"><i class="fa fa-group"></i> Campers</a></li>
+        <li><a href="{{ url('/payment/' . $id) }}"><i class="fa fa-money"></i> Statement</a></li>
+        <li><a href="{{ url('/workshopchoice/' . $id) }}"><i class="fa fa-rocket"></i> Workshops</a></li>
+        <li><a href="{{ url('/roomselection/' . $id) }}"><i class="fa fa-bed"></i> Room</a></li>
+        <li><a href="{{ url('/volunteer/' . $id) }}"><i class="fa fa-handshake-o"></i> Volunteer</a></li>
+        <li><a href="{{ url('/confirm/' . $id) }}"><i class="fa fa-envelope"></i> Confirmation</a></li>
+        <li><a href="{{ url('/nametag/' . $id) }}"><i class="fa fa-id-card"></i> Customize Nametags</a></li>
+        <li><a href="{{ url('/nametags/' . $id) }}"><i class="fa fa-print"></i> Print Nametags</a></li>
+        <li><a href="{{ url('/calendar/' . $id) }}"><i class="fa fa-calendar"></i> Calendar</a></li>
     </ul>
 </div>

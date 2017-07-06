665a3b282fa2a17d8f957a74270edaa811f2492a
Dan Hermann <dh78@me.com>
2017-07-05 20:45:05 -0500

    Sum amount wrong

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -84,7 +84,7 @@ class WelcomeController extends Controller
     {
         return $family != null &&
             \App\Thisyear_Charge::where('familyid', $family->id)->where('chargetypeid', 1003)->
-            where('chargetypeid', 1003)->orWhere('amount', '<', '0')->get()->sum('amount') > 0;
+            where('chargetypeid', 1003)->orWhere('amount', '<', '0')->get()->sum('amount') <= 0;
     }
 
     private

4aa0548c7957271fce342724df89e62355a4f782
Dan Hermann <dh78@me.com>
2017-11-10 22:31:22 -0600

    Homepage refactor

--- resources/views/admin/controls.blade.php
+++ resources/views/admin/controls.blade.php
@@ -3,11 +3,11 @@
             id="controls{{ $id }}" aria-expanded="false">Details <span class="caret"></span></button>
     <ul class="dropdown-menu camperdetails" role="menu" aria-labelledby="controls{{ $id }}">
         <li><a href="{{ url('/household/' . $id) }}"><i class="fa fa-home"></i> Household</a></li>
-        <li><a href="{{ url('/camper/' . $id) }}"><i class="fa fa-group"></i> Campers</a></li>
-        <li><a href="{{ url('/payment/' . $id) }}"><i class="fa fa-money"></i> Statement</a></li>
+        <li><a href="{{ url('/camper/' . $id) }}"><i class="fa fa-users"></i> Campers</a></li>
+        <li><a href="{{ url('/payment/' . $id) }}"><i class="fa fa-usd-circle"></i> Statement</a></li>
         <li><a href="{{ url('/workshopchoice/' . $id) }}"><i class="fa fa-rocket"></i> Workshops</a></li>
         <li><a href="{{ url('/roomselection/' . $id) }}"><i class="fa fa-bed"></i> Room</a></li>
-        <li><a href="{{ url('/volunteer/' . $id) }}"><i class="fa fa-handshake-o"></i> Volunteer</a></li>
+        <li><a href="{{ url('/volunteer/' . $id) }}"><i class="fa fa-handshake"></i> Volunteer</a></li>
         <li><a href="{{ url('/confirm/' . $id) }}"><i class="fa fa-envelope"></i> Confirmation</a></li>
         <li><a href="{{ url('/nametag/' . $id) }}"><i class="fa fa-id-card"></i> Customize Nametags</a></li>
         <li><a href="{{ url('/nametags/' . $id) }}"><i class="fa fa-print"></i> Print Nametags</a></li>

ef2a63c02dbbdac6976311bf20488d890ff46552
Dan Hermann <dh78@me.com>
2017-11-14 10:17:02 -0600

    Login refactor

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,58 +6,59 @@
 
 
 @section('content')
-    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-        {{ csrf_field() }}
+    <p>&nbsp;</p>
+    <div class="container">
+        <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+            @include('snippet.flash')
 
-        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+            <div class="form-group row{{ $errors->has('email') ? ' has-danger' : '' }}">
+                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
-            <div class="col-md-6">
-                <input id="email" type="email" class="form-control" name="email"
-                       value="{{ old('email') }}" required autofocus>
+                <div class="col-md-6">
+                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                           name="email" value="{{ old('email') }}" required autofocus>
 
-                @if ($errors->has('email'))
-                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                @endif
-            </div>
-        </div>
-
-        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-            <label for="password" class="col-md-4 control-label">Password</label>
-
-            <div class="col-md-6">
-                <input id="password" type="password" class="form-control" name="password" required>
-
-                @if ($errors->has('password'))
-                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                @endif
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="col-md-6 col-md-offset-4">
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" name="remember"> Remember Me
-                    </label>
+                    @if ($errors->has('email'))
+                        <span class="invalid-feedback">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
                 </div>
             </div>
-        </div>
 
-        <div class="form-group">
-            <div class="col-md-8 col-md-offset-4">
-                <button type="submit" class="btn btn-primary">
-                    Login
-                </button>
+            <div class="form-group row{{ $errors->has('password') ? ' has-error' : '' }}">
+                <label for="password" class="col-md-4 control-label">Password</label>
 
-                <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                    Forgot Your Password?
-                </a>
+                <div class="col-md-6">
+                    <input id="password" type="password"
+                           class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                           name="password" required>
+
+                    @if ($errors->has('password'))
+                        <span class="invalid-feedback">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
             </div>
-        </div>
-    </form>
+
+            <div class="form-group row">
+                <div class="col-md-4">
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="remember"> Remember Me
+                        </label>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <input type="submit" class="btn btn-lg btn-primary py-3 px-4" value="Login"/>
+
+                    <a class="btn btn-link" href="{{ url('/password/reset') }}">
+                        Forgot Your Password?
+                    </a>
+                </div>
+            </div>
+        </form>
+    </div>
 @endsection

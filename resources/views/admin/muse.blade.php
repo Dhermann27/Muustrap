b344812299ff1a4cde421793f8aa1ebbb98bc9bf
Dan Hermann <dh78@me.com>
2017-11-14 12:08:41 -0600

    Muse upload refactor

--- resources/views/admin/muse.blade.php
+++ resources/views/admin/muse.blade.php
@@ -6,61 +6,50 @@
 
 @section('content')
     @if(Auth::check())
-        <form id="muse" class="form-horizontal" role="form" method="POST"
-              action="{{ url('/museupload') }}" enctype="multipart/form-data">
-            @include('snippet.flash')
+        <div class="container">
+            <form id="muse" class="form-horizontal" role="form" method="POST"
+                  action="{{ url('/museupload') }}" enctype="multipart/form-data">
+                @include('snippet.flash')
 
-            <div class="form-group{{ $errors->has('date') ? ' has-error' : '' }}">
-                <label for="date" class="col-md-4 control-label">Muse Date (yyyy-mm-dd)</label>
-                <div class="col-md-6">
-                    <div class="input-group date" data-provide="datepicker"
-                         data-date-format="yyyy-mm-dd" data-date-autoclose="true">
-                        <input id="date" type="text" class="form-control" name="date"
-                               value="{{ old('date') }}" required>
-                        <div class="input-group-addon">
-                            <span class="fa fa-calendar"></span>
+                <div class="form-group row{{ $errors->has('date') ? ' has-danger' : '' }}">
+                    <label for="date" class="col-md-4 control-label">Muse Date (yyyy-mm-dd)</label>
+                    <div class="col-md-6">
+                        <div class="input-group date" data-provide="datepicker"
+                             data-date-format="yyyy-mm-dd" data-date-autoclose="true">
+                            <input id="date" type="text" class="form-control{{ $errors->has('date') ? ' is-invalid' : '' }}"
+                                   name="date" value="{{ old('date') }}" required>
+                            <div class="input-group-addon">
+                                <span class="fa fa-calendar"></span>
+                            </div>
                         </div>
+                        @if ($errors->has('date'))
+                            <span class="invalid-feedback">
+                                <strong>{{ $errors->first('date') }}</strong>
+                            </span>
+                        @endif
                     </div>
-                    @if ($errors->has('date'))
-                        <span class="help-block">
-                                                <strong>{{ $errors->first('date') }}</strong>
-                                            </span>
-                    @endif
                 </div>
-            </div>
 
-            <div class="form-group{{ $errors->has('images') ? ' has-error' : '' }}">
-                <label for="images" class="col-md-4 control-label">Muse PDF</label>
+                <div class="form-group{{ $errors->has('images') ? ' has-danger' : '' }}">
+                    <label for="images" class="col-md-4 control-label">Muse PDF</label>
 
-                <div class="col-md-6">
-                    <input type="file" name="pdf"/>
+                    <div class="col-md-6">
+                        <input type="file" name="pdf"/>
 
-                    @if ($errors->has('pdf'))
-                        <span class="help-block">
-                                                <strong>{{ $errors->first('pdf') }}</strong>
-                                            </span>
-                    @endif
+                        @if ($errors->has('pdf'))
+                            <span class="invalid-feedback">
+                                <strong>{{ $errors->first('pdf') }}</strong>
+                            </span>
+                        @endif
+                    </div>
                 </div>
-            </div>
 
-            <div class="form-group{{ $errors->has('g-recaptcha-response') ? ' has-error' : '' }}">
-                <div class="col-md-6 col-md-offset-4">
-                    {!! app('captcha')->display() !!}
+                @include('snippet.formgroup', ['type' => 'captcha', 'label' => 'CAPTCHA Test',
+                    'attribs' => ['name' => 'g-recaptcha-response']])
 
-                    @if ($errors->has('g-recaptcha-response'))
-                        <span class="help-block">
-                                        <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
-                                    </span>
-                    @endif
-                </div>
-            </div>
-
-            <div class="form-group">
-                <div class="col-md-6 col-md-offset-4">
-                    <button type="submit" class="btn btn-primary">Upload Muse</button>
-                </div>
-            </div>
-        </form>
+                @include('snippet.formgroup', ['type' => 'submit', 'label' => '', 'attribs' => ['name' => 'Upload Muse']])
+            </form>
+        </div>
     @endif
 @endsection
 

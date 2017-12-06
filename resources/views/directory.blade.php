73e16ac40f12d331d94b89eefca758817e5e8910
Dan Hermann <dh78@me.com>
2017-12-06 13:29:57 -0600

    Directory refactor

--- resources/views/directory.blade.php
+++ resources/views/directory.blade.php
@@ -12,20 +12,21 @@
     <div class="alert alert-info">
         Unfortunately, we do not have historical data from Lake Geneva Summer Assembly.
     </div>
-    <nav aria-label="Page navigation" style="text-align: center;">
-        <ul class="pagination">
-            <li id="previous">
-                <a href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
+    <nav aria-label="Page navigation">
+        <ul class="pagination justify-content-center pt-3">
+            <li class="page-item">
+                <a href="#" class="page-link" id="prev" aria-label="Previous">
+                    <i class="fa fa-chevron-left fa-fw"></i>
                 </a>
             </li>
             @foreach($letters->groupBy('first') as $letter => $families)
-                <li{!! $loop->first ? ' class="active"' : '' !!}><a href="#" class="letterlink">{{ $letter }}</a>
+                <li class="page-item{!! $loop->first ? ' active' : '' !!}">
+                    <a href="#" class="page-link">{{ $letter }}</a>
                 </li>
             @endforeach
-            <li id="next">
-                <a href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
+            <li class="page-item">
+                <a href="#" class="page-link" id="next" aria-label="Next">
+                    <i class="fa fa-chevron-right fa-fw"></i>
                 </a>
             </li>
         </ul>
@@ -74,16 +75,16 @@
 @section('script')
     <script>
         $(function () {
-            $(".letterlink").on('click', function () {
+            $(".page-link:not('#prev,#next')").on('click', function () {
                 $("li.active").removeClass("active");
                 $(this).parent().addClass("active");
                 $(".letterdiv:visible").fadeOut(250);
                 $("#" + $(this).text()).fadeIn();
             });
 
-            $("#previous").on('click', function () {
+            $("#prev").on('click', function () {
                 var active = $('li.active');
-                if (active.prev().attr("id") !== "previous") {
+                if (active.prev().find("a").attr("id") !== "prev") {
                     active.removeClass("active").prev().addClass("active");
                     $(".letterdiv:visible").fadeOut(250).prev().fadeIn(250);
                 }
@@ -91,7 +92,7 @@
 
             $("#next").on('click', function () {
                 var active = $('li.active');
-                if (active.next().attr("id") !== "next") {
+                if (active.next().find("a").attr("id") !== "next") {
                     active.removeClass("active").next().addClass("active");
                     $(".letterdiv:visible").fadeOut(250).next().fadeIn(250);
                 }

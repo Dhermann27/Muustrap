436934812e15c4da23c55f8fbfaa0f29c425b0f5
Dan Hermann <dh78@me.com>
2017-12-06 13:38:21 -0600

    Workshops refactor

--- resources/views/excursions.blade.php
+++ resources/views/excursions.blade.php
@@ -11,15 +11,7 @@
     @foreach($workshops as $workshop)
         <ul class="list-group">
             <li class="list-group-item">
-                @if($workshop->capacity == 999)
-                    <span class="alert alert-success badge">Unlimited Enrollment</span>
-                @elseif($workshop->enrolled >= $workshop->capacity)
-                    <span class="alert alert-danger badge">Waitlist Available</span>
-                @elseif($workshop->enrolled >= ($workshop->capacity * .75))
-                    <span class="alert alert-warning badge">Filling Fast!</span>
-                @else
-                    <span class="alert alert-info badge">Open For Enrollment</span>
-                @endif
+                @include('snippet.filling', ['workshop' => $workshop])
                 <h3>{{ $workshop->name }}</h3>
                 <h5>Led by {{ $workshop->led_by }}</h5>
                 <p>{{ $workshop->blurb }} <i>Days: {{ $workshop->displayDays }}</i></p>
--- resources/views/workshops.blade.php
+++ resources/views/workshops.blade.php
@@ -9,31 +9,18 @@
 @endsection
 
 @section('content')
-    <ul class="nav nav-tabs" role="tablist">
-        @foreach($timeslots as $timeslot)
-            <li role="presentation"{!! $loop->first ? ' class="active"' : '' !!}>
-                <a href="#{{ $timeslot->id }}" aria-controls="{{ $timeslot->id }}" role="tab"
-                   data-toggle="tab">{{ $timeslot->name }}</a></li>
-        @endforeach
-    </ul>
+    @include('snippet.navtabs', ['tabs' => $timeslots, 'id'=> 'id', 'option' => 'name'])
 
     <div class="tab-content">
         @foreach($timeslots as $timeslot)
-            <div role="tabpanel" class="tab-pane fade{{ $loop->first ? ' in active' : '' }}" id="{{ $timeslot->id }}">
+            <div role="tabpanel" class="tab-pane fade{{ $loop->first ? ' active show' : '' }} p-3"
+                 aria-expanded="{{ $loop->first ? 'true' : 'false' }}" id="{{ $timeslot->id }}">
                 <h4>{{ $timeslot->start_time->format('g:i A') }}
                     - {{ $timeslot->end_time->format('g:i A') }}</h4>
                 @foreach($timeslot->workshops as $workshop)
                     <ul class="list-group">
                         <li class="list-group-item">
-                            @if($workshop->capacity == 999)
-                                <span class="alert alert-success badge">Unlimited Enrollment</span>
-                            @elseif($workshop->enrolled >= $workshop->capacity)
-                                <span class="alert alert-danger badge">Workshop Full</span>
-                            @elseif($workshop->enrolled >= ($workshop->capacity * .75))
-                                <span class="alert alert-warning badge">Filling Fast!</span>
-                            @else
-                                <span class="alert alert-info badge">Open For Enrollment</span>
-                            @endif
+                            @include('snippet.filling', ['workshop' => $workshop])
                             <h3>{{ $workshop->name }}</h3>
                             <h5>Led by {{ $workshop->led_by }}</h5>
                             <p>{{ $workshop->blurb }} <i>Days: {{ $workshop->displayDays }}</i></p>

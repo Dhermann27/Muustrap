8940e461720d9976f0ce5bab4f3317ef021ed54b
Dan Hermann <dh78@me.com>
2018-02-01 13:36:50 -0600

    Workshops signup sheets

--- resources/views/reports/workshops.blade.php
+++ resources/views/reports/workshops.blade.php
@@ -29,11 +29,7 @@
                         </thead>
                         <tbody>
                         @foreach($workshop->choices()->orderBy('is_leader', 'desc')->orderBy('created_at')->get() as $choice)
-                            <tr
-                                    @if($choice->is_enrolled == '0')
-                                    style="background-color: indianred;"
-                                    @endif
-                            >
+                            <tr {{ $choice->is_enrolled == '0' ? ' class="table-danger"'}}>
                                 <td>{{ $choice->yearattending->camper->lastname }}
                                     , {{ $choice->yearattending->camper->firstname }}</td>
                                 <td>
@@ -55,7 +51,7 @@
                             </td>
                         </tr>
                         @for($i=count($workshop->choices); $i<min($workshop->capacity, count($workshop->choices)+5); $i++)
-                            <tr class="d-print-block">
+                            <tr class="d-none d-print-block">
                                 <td colspan="2" style="border-bottom: 1px solid black;">&nbsp;</td>
                                 <td>&nbsp;</td>
                             </tr>

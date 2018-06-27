e35616324f24c2ff3efaff9e6bf3fd4fdbb600e7
Dan Hermann <dh78@me.com>
2018-06-27 13:45:16 -0500

    Coffeehouse UI

--- resources/views/coffeehouse.blade.php
+++ resources/views/coffeehouse.blade.php
@@ -117,6 +117,8 @@
 @endsection
 
 @section('script')
+    <script src="//code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+            integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
     <script src="/js/jquery.ui.touch-punch.min.js"></script>
     <script type="text/javascript">
         $("ul.sortable").sortable({
